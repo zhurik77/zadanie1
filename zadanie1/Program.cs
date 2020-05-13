261b02e9fa4fc2d3d53c3f0135c3b403b0f66255
zhurik77 <futureinc.72@yandex.ru>
2020-05-13 23:00:08 +0500

    Добавлено выполненное задание на C#

---
+++ zadanie1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace zadanie1
+{
+    using static lib1;
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(lib1.stpow("ha", 4));
+            Console.WriteLine(lib1.rev("xev"));
+            Console.WriteLine(lib1.remov("xeveer", "e"));
+            Console.WriteLine(lib1.leng("xeveer"));
+        }
+    }
+}
---
+++ zadanie1/lib1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+public class lib1
+{
+    public static string stpow(string a, int b) // Возврат n дублирующей строки
+    {
+        string ready = "";
+        for (int i = 0; i < b; i++)
+        {
+            ready = ready + a;
+        }
+        return ready;
+    }
+    public static string rev(string a) //Возврат перевернутой строки
+    {
+        int length = a.Length;
+        char[] b = a.ToCharArray();
+        string reverse = "";
+        for (int i = length; i != 0; i--)
+        {
+            reverse = reverse + b[i - 1];
+        }
+        return reverse;
+    }
+    public static string remov(string str, string s)// Возврат строки с удалением подстроки
+    {
+        string ready = "";
+        int length_main = str.Length; //Длина строки
+        int length_sec = s.Length;// Длина подстроки
+        int find = str.IndexOf(s);
+        //if (find > -1)
+        //{
+            ready = str.Replace(s, "");
+        //}
+        return ready;
+    }
+    public static int leng(string str)
+    {
+        int length = str.Length;
+        return length;
+    }
+}
+
